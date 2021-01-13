25dcfb21380412dc2f15d3b02399ccc3cbc1077b
Shail Shouryya <shailshouryya@gmail.com>
2021-01-12 22:46:02 -0800

    Add package docstring (__init__.py)

--- python/log_everywhere/__init__.py
+++ python/log_everywhere/__init__.py
@@ -1,2 +1,8 @@
+'''
+Logging package to enable writing the corresponding datetime
+at which every logged event happened to multiple, DIFFERENT files
+safely when running multiple threads
+(multi-threading with python standard library's `threading` package).
+'''
 from .core import yield_logger
 from .core import log

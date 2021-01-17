c8046447385989b04eee6b259efb0ce02475c9de
Shail Shouryya <shailshouryya@gmail.com>
2021-01-16 18:53:54 -0800

    Fix pad bug (reverse booleans)
    - when `not pad` is True, it means `pad` is an empty string
    - therefore previous implementation prepended `pad` to messages when `pad` was unset and  did not prepend `pad` to messages when `pad` was actually set

--- python/log_everywhere/logger.py
+++ python/log_everywhere/logger.py
@@ -47,14 +47,14 @@ def log(message, logging_locations, show_thread=True, show_datetime=True, pad='=
         current_time = isoformat(now())
     # formatted_message keys are a tuple of (pad, show_thread, show_datetime)
     formatted_message = {
-        (True,  True,  True):  f'{pad}{thread_name:>>14} {current_time}: {message}\n',
-        (False, True,  True):  f'{thread_name:>>14} {current_time}: {message}\n',
-        (True,  False, True):  f'{pad}{current_time}: {message}\n',
-        (True,  True,  False): f'{pad}{thread_name:>>14} {message}\n',
-        (False, False, True):  f'{current_time}: {message}\n',
-        (False, True,  False): f'{thread_name:>>14} {message}\n',
-        (True,  False, False): f'{pad} {message}\n',
-        (False, False, False): f'{message}\n',
+        (False,  True,  True):  f'{pad}{thread_name:>>14} {current_time}: {message}\n',
+        (True, True,  True):    f'{thread_name:>>14} {current_time}: {message}\n',
+        (False,  False, True):  f'{pad}{current_time}: {message}\n',
+        (False,  True,  False): f'{pad}{thread_name:>>14} {message}\n',
+        (True, False, True):    f'{current_time}: {message}\n',
+        (True, True,  False):   f'{thread_name:>>14} {message}\n',
+        (False,  False, False): f'{pad} {message}\n',
+        (True, False, False):   f'{message}\n',
     }
     message = formatted_message[(not pad, show_thread, show_datetime)]
     for location in logging_locations:
